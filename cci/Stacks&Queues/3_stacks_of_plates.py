17d552ab077e511cc49556fa2fe9bc200eff93f4
Srikar K <sclifeisgreat@gmail.com>
2024-10-30 07:17:54 -0700

    stacks_of_plates

---
+++ cci/Stacks&Queues/3_stacks_of_plates.py
@@ -0,0 +1,31 @@
+class SetOfStacks:
+    def __init__(self, capacity):
+        self.stacks = []
+        self.capacity = capacity
+
+    def push(self, value):
+        if not self.stacks or len(self.stacks[-1] == self.capacity):
+            self.stacks.append([])
+        self.stacks[-1].append(value)
+
+    def pop(self):
+        if not self.stacks:
+            raise Exception("Stack empty")
+        
+        value = self.stacks[-1].pop()
+
+        if len(self.stacks[-1]) == 0:
+            self.stacks.pop()
+
+        return value
+    
+    def pop_at(self, index):
+        if index < 0 or index >= len(self.stacks):
+            raise Exception("Invalid stack index")
+        
+        value = self.stacks[index].pop()
+
+        if len(self.stacks[index]) == 0:
+            self.stacks.pop(index)
+
+        return value
