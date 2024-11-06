5c49c741681d2395542fb57a4d249afa95cfedac
Srikar K <sclifeisgreat@gmail.com>
2024-11-05 21:12:31 -0800

    check_balanced

---
+++ cci/Trees&Graphs/4_check_balanced.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def is_balanced(root):
+
+    def check_balance(node):
+        if not node:
+            return 0
+        
+        left_height = check_balance(node.left)
+        if left_height == -1:
+            return -1
+        
+        right_height = check_balance(node.right)
+        if right_height == -1:
+            return -1
+        
+        if abs(left_height - right_height) > 1:
+            return -1
+        
+        return max(left_height, right_height) + 1
+    
+    return check_balance(root) != -1
